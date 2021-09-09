3a300090318c4b08160b0becd82f05f180394aad
Tyler Carter <70717266+TC5555@users.noreply.github.com>
2021-09-09 10:01:56 -0700

    Initial commit

---
+++ src/therewasanoldlady.java
@@ -0,0 +1,82 @@
+public class therewasanoldlady {
+    public static void verse1() {
+        System.out.println("There was an old lady who swallowed a fly.\n" +
+                "I dunno why she swallowed that fly,\n" +
+                "Perhaps she'll die.\n");
+    }
+    public static void verse2() {
+        System.out.println("There was an old lady who swallowed a spider,\n" +
+                "That wiggled and wiggled and tickled inside her.\n" +
+                "She swallowed the spider to catch the fly.\n" +
+                "But I dunno why she swallowed that fly -\n" +
+                "Perhaps she'll die.\n");
+    }
+    public static void verse3() {
+        System.out.println("There was an old lady who swallowed a bird;\n" +
+                "How absurd, to swallow a bird!\n" +
+                "She swallowed the bird to catch the spider\n" +
+                "That wiggled and wiggled and tickled inside her.\n" +
+                "She swallowed the spider to catch the fly.\n" +
+                "But I dunno why she swallowed that fly -\n" +
+                "Perhaps she'll die.\n");
+    }
+    public static void verse4() {
+        System.out.println("There was an old lady who swallowed a cat.\n" +
+                "Imagine that, she swallowed a cat.\n" +
+                "She swallowed the cat to catch the bird ...\n" +
+                "She swallowed the bird to catch the spider\n" +
+                "That wiggled and wiggled and tickled inside her.\n" +
+                "She swallowed the spider to catch the fly.\n" +
+                "But I dunno why she swallowed that fly\n" +
+                "Perhaps she'll die.\n");
+    }
+    public static void verse5() {
+        System.out.println("There was an old lady who swallowed a dog.\n" +
+                "What a hog! To swallow a dog!\n" +
+                "She swallowed the dog to catch the cat...\n" +
+                "She swallowed the cat to catch the bird ...\n" +
+                "She swallowed the bird to catch the spider\n" +
+                "That wiggled and wiggled and tickled inside her.\n" +
+                "She swallowed the spider to catch the fly.\n" +
+                "But I dunno why she swallowed that fly\n" +
+                "Perhaps she'll die.\n");
+    }
+    public static void verse6() {
+        System.out.println("There was an old lady who swallowed a goat.\n" +
+                "Just opened her throat and swallowed a goat!\n" +
+                "She swallowed the goat to catch the dog ...\n" +
+                "She swallowed the dog to catch the cat.\n" +
+                "She swallowed the cat to catch the bird ...\n" +
+                "She swallowed the bird to catch the spider\n" +
+                "That wiggled and wiggled and tickled inside her.\n" +
+                "She swallowed the spider to catch the fly.\n" +
+                "But I dunno why she swallowed that fly\n" +
+                "Perhaps she'll die.\n");
+    }
+    public static void verse7() {
+        System.out.println("There was an old lady who swallowed a cow.\n" +
+                "I don't know how she swallowed a cow!\n" +
+                "She swallowed the cow to catch the goat...\n" +
+                "She swallowed the goat to catch the dog...\n" +
+                "She swallowed the dog to catch the cat...\n" +
+                "She swallowed the cat to catch the bird ...\n" +
+                "She swallowed the bird to catch the spider\n" +
+                "That wiggled and wiggled and tickled inside her.\n" +
+                "She swallowed the spider to catch the fly.\n" +
+                "But I dunno why she swallowed that fly\n" +
+                "Perhaps she'll die.\n");
+    }
+    public static void verse8() {
+        System.out.println("There was an old lady who swallowed a horse -\n" +
+                "She's dead, of course.\n");
+    }
+    public static void main(String[] args) {
+        verse1();
+        verse2();
+        verse3();
+        verse4();
+        verse5();
+        verse6();
+        verse7();
+    }
+}
